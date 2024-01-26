76b292be69329a1fdc8fc878a2a10b1479477e63
Joao Gabriel <jgkasprik13@gmail.com>
2024-01-25 22:36:32 -0300

    Initial commit

---
+++ src/main/java/com/moterlode/api/MoterlodeApplication.java
@@ -0,0 +1,13 @@
+package com.moterlode.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoterlodeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoterlodeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/moterlode/api/MoterlodeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.moterlode.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoterlodeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
